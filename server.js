15a09b6578f5ee8d54259064bffcd1ec8faaa187
thuongmai <thuongmai10292@gmail.com>
2024-03-11 23:20:51 -0400

    [backend_framework] Add generic framework to call API

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Database connection
+
+// Middleware
+
+// API Routes
+
+// Start server
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
