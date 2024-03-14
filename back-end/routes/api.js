e9e65a56303a0fb391490dab355de4e6f6606eac
thuongmai <thuongmai10292@gmail.com>
2024-03-13 20:39:45 -0400

    Add POST API test + lodash

--- back-end/routes/api.js
+++ back-end/routes/api.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const _ = require('lodash');
 const router = express.Router();
 const UserController = require('../controllers/UserController');
 
@@ -6,6 +7,17 @@ const UserController = require('../controllers/UserController');
 router.get('/', (req, res) => {
 	res.send('Hello World!');
 });
+router.post('/', (req, res) => {
+	// Extract the request body
+	const requestBody = req.body;
+
+	if (_.isEmpty(requestBody)) {
+		res.status(404).json({ message: 'POST request not received' });
+	}
+
+	// Return the request body in the response
+	res.json({ message: 'POST request received successfully', requestBody });
+})
 
 // Login route
 router.post('/login', UserController.login);
